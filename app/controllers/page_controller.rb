c82cdb9c129b13b70fbdf4e0390e0f7feded1b1d
Eduard <kuliev.ed@gmail.com>
2018-12-21 21:58:03 +0300

    upgrade menu

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,25 +1,25 @@
-# class PageController < ApplicationController
-#   def index; end
-# end
-#
-#
-# class CompileController < ActionController::API
-#   require 'tilt/kramdown'
-#
-#   def pdf
-#     File.open(dir.join('note.pdf'), 'wb') do |f|
-#       send_file f, filename: 'note.pdf'
-#     end
-#   end
-#
-#   def compile
-#     text = params['text'].nil? ? '\n' : params['text']
-#     pdf = Kramdown::Document.new(text, input: 'kramdown').to_pdf
-#     dir = Rails.root.join('public', 'notes')
-#     Dir.mkdir(dir) unless Dir.exist?(dir)
-#     File.open(dir.join('note.pdf'), 'wb') do |file|
-#       file.write(pdf)
-#     end
-#   end
-# end
-#
+class PageController < ApplicationController
+  def index; end
+end
+
+
+class CompileController < ActionController::API
+  require 'tilt/kramdown'
+
+  def pdf
+    File.open(dir.join('note.pdf'), 'wb') do |f|
+      send_file f, filename: 'note.pdf'
+    end
+  end
+
+  def compile
+    text = params['text'].nil? ? '\n' : params['text']
+    pdf = Kramdown::Document.new(text, input: 'kramdown').to_pdf
+    dir = Rails.root.join('public', 'notes')
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    File.open(dir.join('note.pdf'), 'wb') do |file|
+      file.write(pdf)
+    end
+  end
+end
+
